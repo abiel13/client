884a8ad1e7efd1aef4f7597e022f51778ba6ab47
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-21 15:46:55 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar({ search, setSsearch, user }) {
   if (!user) return null;
   return (
     <div className="flex gap-2 mt-5 pb-7 md:gap-5">
-      <div className="justify-start items-center w-full bg-white px-2 rounded-md border-none outline-none focus-within:shadow-sm">
+      <div className="justify-start flex items-center w-full bg-white px-2 rounded-md border-none outline-none focus-within:shadow-sm">
         <IoMdSearch fontSize={20} />
         <input
           type="text"
