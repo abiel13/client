3bd8445e32a631031dab2508eda4358cba8e6790
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-21 08:53:39 -0700

    Update Pages.jsx

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import { Login } from './'
-import {Home } from '../container/Home'
+import Home from '../container/Home'
 function Pages() {
   return (
     <BrowserRouter>
